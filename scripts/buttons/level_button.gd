b9fdcc833ce070f182cf58cda97f9cb06f7741e8
Simone Starace <simone.starace93@gmail.com>
2023-09-17 11:33:57 +0200

    Fix reference to levels

--- scripts/buttons/level_button.gd
+++ scripts/buttons/level_button.gd
@@ -2,13 +2,9 @@ class_name LevelButton extends Button
 
 ## Button for loading levels
 
-## Scene to load
-@export var level: PackedScene
-
 func _ready():
-	self.pressed.connect(_load_level)
+	self.pressed.connect(_reload_game)
 
-## Loads a specific scene
-func _load_level():
-	if level:
-		get_tree().change_scene_to_packed(level)
+## Loads the first level
+func _reload_game():
+	get_tree().change_scene_to_file("res://scenes/levels/level_1.tscn")
